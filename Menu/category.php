886e4cb37fe4bacb552a5219541fd0cbc6c951e0
Nguyễn Phương Hải <102780418+hainp993@users.noreply.github.com>
2023-06-30 09:29:50 +0700

    Update category.php

--- Menu/category.php
+++ Menu/category.php
@@ -27,8 +27,8 @@ $num_row = mysqli_num_rows($query_category)
         ?>
         <div class="col-lg-4 col-md-6 col-sm-12 mx-product">
             <div class="product-item card text-center">
-                <a href="#"><img src="admin/images/<?php echo $item['prd_image']; ?>"></a>
-                <h4><a href="#"><?php echo $item['prd_name']; ?></a></h4>
+                <a href="index.php?redirect=product&prd_id=<?php echo $item['prd_id'] ?>"><img src="admin/images/<?php echo $item['prd_image']; ?>"></a>
+                <h4><a href="index.php?redirect=product&prd_id=<?php echo $item['prd_id'] ?>"><?php echo $item['prd_name']; ?></a></h4>
                 <p>Giá Bán: <span><?php echo number_format($item['prd_price']); ?>đ</span></p>
             </div>
         </div>
